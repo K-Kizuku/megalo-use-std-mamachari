d8bfef5c41a306f3c6abb1d4b3c1e7a50179bfee
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-12-17 13:06:59 +0900

    init: chat server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,67 @@
-use actix_web::{web, HttpServer, HttpResponse, App, Responder};
+use std::{
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::Instant,
+};
+
+use actix::*;
+use actix_files::{Files, NamedFile};
+use actix_web::{
+    middleware::Logger, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use actix_web_actors::ws::{self, start};
+
+use megalo_use_std_mamachari::chat_server;
+use megalo_use_std_mamachari::chat_session;
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+/// Entry point for our websocket route
+async fn chat_route(
+    req: HttpRequest,
+    stream: web::Payload,
+    srv: web::Data<Addr<chat_server::ChatServer>>,
+) -> Result<HttpResponse, Error> {
+    ws::start(
+        chat_session::WsChatSession {
+            id: 0,
+            hb: Instant::now(),
+            room: "main".to_owned(),
+            name: None,
+            addr: srv.get_ref().clone(),
+        },
+        &req,
+        stream,
+    )
+}
+
+async fn get_count(count: web::Data<AtomicUsize>) -> impl Responder {
+    let current_count = count.load(Ordering::SeqCst);
+    format!("Visitors: {current_count}")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_state = Arc::new(AtomicUsize::new(0));
+    let chat_server = chat_server::ChatServer::new(app_state.clone()).start();
+
+    HttpServer::new(move || {
         App::new()
-            .route("/", web::get().to(index))
+            .app_data(web::Data::from(app_state.clone()))
+            .app_data(web::Data::new(chat_server.clone()))
+            .service(web::resource("/").to(index))
+            .route("/count", web::get().to(get_count))
+            .route("/ws", web::get().to(chat_route))
+            .service(Files::new("/static", "./static"))
+            .wrap(Logger::default())
     })
-    .bind("127.0.0.1:8080")?
+    .workers(2)
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
+
