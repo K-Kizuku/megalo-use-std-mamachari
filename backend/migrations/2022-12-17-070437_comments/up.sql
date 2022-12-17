fed5cfbf8ed2981e1f347cc540aa3a637d7ed8c0
yamato0211 <kumayama0211@gmail.com>
2022-12-17 16:17:23 +0900

    [add] テーブル作成

---
+++ backend/migrations/2022-12-17-070437_comments/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+create table comments(
+    id varchar not null primary key default gen_random_uuid(),
+    user_id varchar not null,
+    stream_id varchar not null,
+    content varchar not null,
+    created_at timestamp default now(),
+    foreign key (user_id) references users(id),
+    foreign key (stream_id) references streams(id)
+);
---
+++ backend/src/schema.rs
@@ -0,0 +1,55 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    comments (id) {
+        id -> Varchar,
+        user_id -> Varchar,
+        stream_id -> Varchar,
+        content -> Varchar,
+        created_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
+    favorites (user_id, stream_id) {
+        user_id -> Varchar,
+        stream_id -> Varchar,
+        content -> Varchar,
+    }
+}
+
+diesel::table! {
+    streams (id) {
+        id -> Varchar,
+        streamed_by -> Varchar,
+        title -> Varchar,
+        description -> Varchar,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        id -> Varchar,
+        name -> Varchar,
+        email -> Varchar,
+        description -> Varchar,
+        password -> Varchar,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::joinable!(comments -> streams (stream_id));
+diesel::joinable!(comments -> users (user_id));
+diesel::joinable!(favorites -> streams (stream_id));
+diesel::joinable!(favorites -> users (user_id));
+diesel::joinable!(streams -> users (streamed_by));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    comments,
+    favorites,
+    streams,
+    users,
+);
