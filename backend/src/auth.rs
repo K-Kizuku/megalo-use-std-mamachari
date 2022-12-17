33f8183242032e92474c21177379cc9f7fb5f3be
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-12-18 00:36:10 +0900

    add: firebase auth

--- backend/src/auth.rs
+++ backend/src/auth.rs
@@ -1,48 +1,39 @@
-use crate::errors::ServiceError;
-use alcoholic_jwt::{token_kid, validate, Validation, JWKS};
-use serde::{Deserialize, Serialize};
-use std::error::Error;
+use log::info;
+use actix_web::{HttpRequest, HttpResponse, Responder, web, HttpResponseBuilder};
+use fireauth::FireAuth;
+use serde::Deserialize;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Claims {
-    sub: String,
-    company: String,
-    exp: usize,
+#[derive(Deserialize)]
+pub struct User {
+    email: String,
+    password: String,
 }
 
-pub async fn validate_token(token: &str) -> Result<bool, ServiceError> {
-    let jwk_url = std::env::var("JWK_URL").expect("JWK_URL must be set");
-    let jwk_issuer = std::env::var("JWK_ISSUER").expect("JWK_ISSUER must be set");
-    println!("jwk_url:{}", jwk_url);
-    println!("jwk_issuer:[{}]", jwk_issuer);
-
-    let jwks = fetch_jwks(jwk_url.as_str())
-        .await
-        .expect("failed to fetch jwks");
-
-    // issとsub（user_id）をチェック
-    // 必須ではなく、カスタマイズできる
-    let validations = vec![Validation::Issuer(jwk_issuer), Validation::SubjectPresent];
-
-    let kid = match token_kid(token) {
-        Ok(res) => res.expect("failed to decode kid"),
-        Err(_) => return Err(ServiceError::JWKSFetchError),
+pub async fn firebase_signup(payload: web::Json<User>) -> impl Responder{
+    let api_key: String = std::env::var("FIREBASE_API").expect("FIREBASE_API does not exist !");
+    let auth = FireAuth::new(api_key);
+    let email = &payload.email;
+    let password = &payload.password;
+    let responce = match auth.sign_up_email(&email, &password, true).await {
+        Ok(response) => response,
+        Err(_) => return HttpResponse::Unauthorized().finish(),
     };
-
-    println!("kid:{:?}", kid);
-
-    let jwk = jwks.find(&kid).expect("Specified key not found in set");
-
-    println!("jwk:{:?}", jwk);
-
-    let res = validate(token, jwk, validations);
-    println!("res.is_ok():{}", res.is_ok());
-    Ok(res.is_ok())
+    info!("id_token: {:?}", responce.id_token);
+    info!("email: {:?}", responce.email);
+    HttpResponse::Ok().body("OK")
 }
 
-async fn fetch_jwks(uri: &str) -> Result<JWKS, Box<dyn Error>> {
-    let res = reqwest::get(uri).await?;
-    let val = res.json::<JWKS>().await?;
-    Ok(val)
+pub async fn firebase_signin(payload: web::Json<User>) -> impl Responder{
+    let api_key: String = std::env::var("FIREBASE_API").expect("FIREBASE_API does not exist !");
+    let auth = FireAuth::new(api_key);
+    let email = &payload.email;
+    let password = &payload.password;
+    let responce = match auth.sign_in_email(&email, &password, true).await {
+        Ok(response) => response,
+        Err(_) => return HttpResponse::Unauthorized().finish(),
+    };
+    info!("id_token: {:?}", responce.id_token);
+    info!("email: {:?}", responce.email);
+    HttpResponse::Ok().body("OK")
 }
 
