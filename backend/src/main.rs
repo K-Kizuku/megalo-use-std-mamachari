1000aa853e78a92b60d8a700a843e16a6bb6b551
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-12-17 14:16:54 +0900

    add: chat page / websocket and route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,14 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger as logger;
+
 use std::{
     sync::{
         atomic::{AtomicUsize, Ordering},
         Arc,
     },
     time::Instant,
+    env,
 };
 
 use actix::*;
@@ -13,11 +18,17 @@ use actix_web::{
 };
 use actix_web_actors::ws::{self, start};
 
+use log::{Level, logger};
+
 use megalo_use_std_mamachari::chat_server;
 use megalo_use_std_mamachari::chat_session;
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+async fn chat_index() -> impl Responder {
+    NamedFile::open_async("./test/chat.html").await.unwrap()
 }
 
 /// Entry point for our websocket route
@@ -46,17 +57,21 @@ async fn get_count(count: web::Data<AtomicUsize>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    //env::set_var("RUST_LOG", "trace");
+    env::set_var("RUST_LOG", "info");
+    logger::init();
     let app_state = Arc::new(AtomicUsize::new(0));
     let chat_server = chat_server::ChatServer::new(app_state.clone()).start();
-
+    info!("HTTP Server Started at http://localhost:8080");
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::from(app_state.clone()))
             .app_data(web::Data::new(chat_server.clone()))
             .service(web::resource("/").to(index))
-            .route("/count", web::get().to(get_count))
-            .route("/ws", web::get().to(chat_route))
-            .service(Files::new("/static", "./static"))
+            .service(web::resource("/chat").to(chat_index))
+            .route("/chat_count", web::get().to(get_count))
+            .route("/chat_ws", web::get().to(chat_route))
+            .service(Files::new("/test", "./test"))
             .wrap(Logger::default())
     })
     .workers(2)
